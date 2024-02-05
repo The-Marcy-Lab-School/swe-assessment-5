4846b7d79bd9e72b4245da3ea480d135a8d409c1
benspector-mls <ben@marcylabschool.org>
2024-02-05 10:21:30 -0500

    tests

--- src/from-scratch.spec.js
+++ src/from-scratch.spec.js
@@ -10,7 +10,7 @@ const testSuiteName = 'From Scratch Tests';
 const scoresDir = path.join(__dirname, '..', 'scores');
 const scoreCounter = new ScoreCounter(testSuiteName, scoresDir);
 
-const log = jest.spyOn(console, 'log').mockImplementation(() => {});
+const log = jest.spyOn(console, 'log').mockImplementation(() => { });
 
 describe(testSuiteName, () => {
   afterEach(() => {
@@ -32,6 +32,12 @@ describe(testSuiteName, () => {
     const myAccount = bankAccountFactory(1000);
     expect(myAccount.checkBalance()).toEqual(1000);
 
+    const account2 = bankAccountFactory(50);
+    expect(account2.checkBalance()).toEqual(50);
+
+    const account3 = bankAccountFactory(500);
+    expect(account3.checkBalance()).toEqual(500);
+
     scoreCounter.correct(expect); // DO NOT TOUCH
   });
 
@@ -43,6 +49,7 @@ describe(testSuiteName, () => {
   });
 
   it('bankAccountFactory - add(): increases the balance and console logs a message', () => {
+    // we randomize the values when testing to ensure that your solutions aren't hard-coded
     const startingBalance = Math.floor(Math.random() * 1000);
     const firstAmount = Math.floor(Math.random() * 1000);
     const secondAmount = Math.floor(Math.random() * 1000);
