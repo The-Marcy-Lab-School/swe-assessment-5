0a41da8670418be0898ba3f6ee61960d5bd0bb13
Ben Spector <111444562+benspector-mls@users.noreply.github.com>
2025-02-13 09:43:01 -0500

    Update 1-has-many-belongs-to.js

--- src/1-has-many-belongs-to.js
+++ src/1-has-many-belongs-to.js
@@ -1,16 +1,12 @@
 const { getId } = require('./utils');
 
 class ToDoItem {
-  constructor() {
-    this.id = getId();
-  }
+ 
 }
 
 
 class ToDoList {
-  constructor() {
-    this.id = getId();
-  }
+
 }
 
 module.exports = {
